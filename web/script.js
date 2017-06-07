7375984c4395ebd18359c3fa8c614e000d2260c8
baku <fhzotxkdj@gmail.com>
2017-06-07 04:28:55 -0400

    web

--- web/script.js
+++ web/script.js
@@ -1,11 +1,30 @@
 $(document).ready(function() {
+  var toggle = 0;
+
   $(".sendMail").click(function() {
     console.log("fucl");
     var email = 'probaku1234@naver.com';
     var subject = 'Test';
     var emailBody = 'Hi Sample,';
     var attach = 'path';
-
     document.location = "mailto:" + email;
+  });
+
+  $("#authenBtn").click(function() {
+    var key = prompt("Please enter the key", "key");
+  });
+
+  $(".menuWrapper").click(function() {
+    this.classList.toggle("change");
+    if (toggle == 0)
+    {
+      $(".modal").fadeIn();
+      toggle = 1;
+    }
+    else
+    {
+      $(".modal").fadeOut();
+      toggle = 0;
+    }
   });
 });
